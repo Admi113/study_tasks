00e3ff7546ca12d937cddc25facbb7e8b3c1c254
Admi113 <nika1pb@mail.ru>
2021-08-18 15:06:42 +0300

    Initial commit

---
+++ src/main/java/ru/diasoft/study_tasks/StudyTasksApplication.java
@@ -0,0 +1,13 @@
+package ru.diasoft.study_tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyTasksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyTasksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/diasoft/study_tasks/StudyTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.diasoft.study_tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyTasksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
